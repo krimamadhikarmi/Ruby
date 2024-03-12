07801bfebb920c51dc2881ab3e6f1835072b7482
krimamadhikarmi <madhikrima20@gmail.com>
2024-03-12 14:02:51 +0545

    loops

--- loops.rb
+++ loops.rb
@@ -1,3 +1,4 @@
+#each loop
 numbers=[1,2,3,4]
 numbers.each do |i| #|i| is a block arg it is valid only within do and end
     puts i 
@@ -10,7 +11,57 @@ numbers.each do |i|
     puts i * 3 #each number multiplied by 2
 end
 
+countries=[]
+countries.push('Nepal')
+countries.push('India')
+countries.push('China')
+puts countries
+
+countries.each do |i|
+    puts "#{i} is  in Asia"
+end
 
 
+#while loop
+i=0
+while (i<5) do
+    i+=1
+    puts i
+end
 
+#times loop
+5.times do |i|
+    puts i
+end
+
+1.upto(9) do |i|
+    puts i
+end
+
+#downto
+10.downto(2) do |n|
+    puts n
+end
+
+#challenges
+puts "Enter your sentence"
+count=0
+content=[]
+sentence=gets.chomp
+sentence=sentence.split(" ")
+puts sentence
+
+
+sentence.each do |i|  #[hello i am hello]
+    sentence.each do|j| 
+       if (i == j) && !(content.include?(i))
+         count+=1
+       end
+    end
+    if !(content.include?(i))
+        content.push(i)
+        puts "#{i}=#{count}"
+    end
+    count=0
+end
 
