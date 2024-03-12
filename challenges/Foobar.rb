2ab631d6ad854296a18acfe9ce71e0cfc89d02a7
krimamadhikarmi <madhikrima20@gmail.com>
2024-03-12 23:03:37 +0545

    Foobar

--- challenges/Foobar.rb
+++ challenges/Foobar.rb
@@ -1,18 +1,17 @@
 puts "Enter the limit upto which you want to print"
 num=gets.chomp.to_i
-res=[]
-1.upto(num) do |i|
+
+1.upto (num) do |i|
     if i%3==0 && i%5==0
-        res.push("Foobar")
+        puts "Foobar"
     elsif i%3==0
-        res.push("Foo")
+         puts "Foo" 
     elsif i%5==0
-        res.push("bar")
+        puts "Bar"
     else
-       res.push(i)
+        puts i
     end
 end
-puts res
 
 #using method
 puts "Enter the number limit you want to print"
